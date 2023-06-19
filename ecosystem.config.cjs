6ddb3eaa1f9517993b7297c4432b9124202ab856
Steven van Zelst <steven@digitalnatives.nl>
2023-06-19 18:00:02 +0200

    Restart the process

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,9 @@ module.exports = {
             name: 'staging',
             script: './dist/server/entry.mjs',
             autorestart: true,
+            watch: true,
             max_memory_restart: '1G',
+            exec_mode: 'cluster',
             env: {
                 NODE_ENV: 'production',
                 APP_ENV: 'staging',
