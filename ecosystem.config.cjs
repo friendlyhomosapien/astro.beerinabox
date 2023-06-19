523cc8dc441c6182e5813f8ca9f452ab29418393
Steven van Zelst <steven@digitalnatives.nl>
2023-06-19 18:16:58 +0200

    Set the right path for PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,10 +3,15 @@ module.exports = {
         {
             name: 'staging',
             script: './dist/server/entry.mjs',
-            autorestart: true,
-            watch: true,
+            cwd: `/home/ploi/beerinabox.dev`,
+            instances: 'max',
+            wait_ready: true,
             max_memory_restart: '1G',
+            listen_timeout: 5000,
             exec_mode: 'cluster',
+            error_file: '../../logs/app.err.log',
+            out_file: '../../logs/app.out.log',
+            script: './.output/server/index.mjs',
             env: {
                 NODE_ENV: 'production',
                 APP_ENV: 'staging',
